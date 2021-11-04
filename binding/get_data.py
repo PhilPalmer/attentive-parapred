65b5884ae1d86113f8c982e776426ae45abbc8cc
PhilPalmer <phil.palmer.email@gmail.com>
2021-11-04 17:05:11 +0000

    Replaced func to download single pdb with biopy pdblist

--- binding/get_data.py
+++ binding/get_data.py
@@ -1,13 +1,14 @@
-import os.path
+import Bio
+from Bio.PDB import PDBList
+import os
 import pandas as pd
-import sys
 import urllib.request
 
 from constants import *
 
 def html_to_csv(download_url, csv_fname):
   """
-  Extract tabular data from a HTML webpage and save as CSV
+  Extract tabular data from a HTML webpage and save as CSV.
   :param download_url: URL of HTML webpage to download containing tabular data
   :param csv_fname: Output CSV file path
   """
@@ -15,24 +16,16 @@ def html_to_csv(download_url, csv_fname):
   df = pd.read_html(html)[-1]
   df.to_csv(csv_fname)
 
-def download_pdb(pdb_id, data_dir, download_url="https://files.rcsb.org/download/"):
-    """
-    Downloads a PDB file from the Internet and saves it in a data directory.
-    :param pdb_id: The standard PDB ID e.g. '3ICB' or '3icb'
-    :param data_dir: The directory where the downloaded file will be saved
-    :param download_url: The base PDB download URL, cf.
-        `https://www.rcsb.org/pages/download/http#structures` for details
-    :return: the full path to the downloaded PDB file or None if something went wrong
-    """
-    pdb_fname = pdb_id + ".pdb"
-    url = download_url + pdb_fname
-    out_fname = os.path.join(data_dir, pdb_fname)
-    try:
-        urllib.request.urlretrieve(url, out_fname)
-        return out_fname
-    except Exception as err:
-        print(str(err), file=sys.stderr)
-        return None
+def download_pdbs(pdb_list, data_dir):
+  """
+  Download PDB files and save in the specified data directory.
+  :param pdb_list: A list of standard PDB IDs to download e.g. `['4B97','4IPH','4HNO']`
+  :param data_dir: The directory where the downloaded file will be saved
+  """
+  pdbl = PDBList()
+  for pdb_code in pdb_list:
+      pdb_fname = pdbl.retrieve_pdb_file(pdb_code, pdir=data_dir, file_format='pdb')
+      os.rename(pdb_fname, f"{data_dir}/{pdb_code}.pdb")
 
 if __name__ == '__main__':
   if not os.path.exists(AFFINITY_CSV):
