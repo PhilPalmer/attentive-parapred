389fce0a0e1bd993410758894916ab1a07201b2b
PhilPalmer <phil.palmer.email@gmail.com>
2021-11-04 17:26:01 +0000

    Added download_data func to download affinity data

--- binding/constants.py
+++ binding/constants.py
@@ -4,3 +4,4 @@ Constants used for the binding prediction
 
 AFFINITY_URL = 'https://bmm.crick.ac.uk/~bmmadmin/Affinity/display_table2.cgi?sort=DeltaG&order=asc'
 AFFINITY_CSV = 'data/affinity_db.csv'
+DATA_DIRECTORY = 'data/'
--- binding/get_data.py
+++ binding/get_data.py
@@ -27,6 +27,18 @@ def download_pdbs(pdb_list, data_dir):
       pdb_fname = pdbl.retrieve_pdb_file(pdb_code, pdir=data_dir, file_format='pdb')
       os.rename(pdb_fname, f"{data_dir}/{pdb_code}.pdb")
 
+def download_data(data_dir=DATA_DIRECTORY, affinity_url=AFFINITY_URL, affinity_csv=AFFINITY_CSV):
+  """
+  Download Affinity DB and associated PDB files if they don't already exist in the specified data directory.
+  :param data_dir: The directory where the downloaded file will be saved
+  :param affinity_url: URL of the Affinity DB HTML webpage to download containing tabular data
+  :param affinity_csv: Affinity DB CSV file path
+  """
+  if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+  if not os.path.exists(affinity_csv):
+    html_to_csv(affinity_url, affinity_csv)
+
 if __name__ == '__main__':
   if not os.path.exists(AFFINITY_CSV):
     html_to_csv(AFFINITY_URL, AFFINITY_CSV)
